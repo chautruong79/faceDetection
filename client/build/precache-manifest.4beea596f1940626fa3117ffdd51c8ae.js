e760a199fda0e998d93da72d878b2933375435cc
chautruong79 <62908290+chautruong79@users.noreply.github.com>
2020-06-11 22:07:37 -0600

    Create precache-manifest.4beea596f1940626fa3117ffdd51c8ae.js

---
+++ client/build/precache-manifest.4beea596f1940626fa3117ffdd51c8ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5fa309ca091a05146f28616483d2dc",
+    "url": "/faceDetection/index.html"
+  },
+  {
+    "revision": "a64694f181db7ebf7bb6",
+    "url": "/faceDetection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1e188b3c7c00f12ccf86",
+    "url": "/faceDetection/static/css/main.c214c994.chunk.css"
+  },
+  {
+    "revision": "a64694f181db7ebf7bb6",
+    "url": "/faceDetection/static/js/2.4bb34eb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faceDetection/static/js/2.4bb34eb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e188b3c7c00f12ccf86",
+    "url": "/faceDetection/static/js/main.7765eaf6.chunk.js"
+  },
+  {
+    "revision": "1c9e037d5e2e03d5fb58",
+    "url": "/faceDetection/static/js/runtime-main.8cca2087.js"
+  },
+  {
+    "revision": "fc0849b9154525fcf8337a5216a2594c",
+    "url": "/faceDetection/static/media/bg.fc0849b9.jpg"
+  }
+]);
